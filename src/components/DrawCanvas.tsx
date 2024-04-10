48bae9bb3607c8c4c105139c42b86f456c497f3a
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-04-09 18:16:27 -0700

    Disable context menu on canvas

--- src/components/DrawCanvas.tsx
+++ src/components/DrawCanvas.tsx
@@ -1,7 +1,16 @@
 import { memo, forwardRef } from "react"
 
 function _DrawCanvas(_: any, ref: React.ForwardedRef<HTMLCanvasElement>) {
-  return <canvas ref={ref} className="h-full w-full cursor-crosshair touch-none" />
+  return (
+    <canvas
+      ref={ref}
+      onContextMenu={(event) => {
+        event.preventDefault()
+        return false
+      }}
+      className="h-full w-full cursor-crosshair touch-none select-none"
+    />
+  )
 }
 
 export const DrawCanvas = memo(forwardRef(_DrawCanvas))
