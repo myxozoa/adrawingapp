efd804d1327e4ca0e14fac4cf7149678f1eb04d8
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-05-15 20:42:11 -0700

    initial export dialog with dropdown for file types and quality slider

---
+++ src/components/ExportDialog.tsx
@@ -0,0 +1,140 @@
+import { useState } from "react"
+
+import { Application } from "@/managers/ApplicationManager"
+import { ResourceManager } from "@/managers/ResourceManager"
+import { usePreferenceStore } from "@/stores/PreferenceStore"
+import { DrawingManager } from "@/managers/DrawingManager"
+import { Button } from "@/components/ui/button"
+
+import { DialogHeader, DialogTitle } from "@/components/ui/dialog"
+
+import { SettingSlider } from "@/components/SettingSlider"
+
+import { readPixelsAsync } from "@/utils/asyncReadback"
+
+import { getMIMEFromImageExtension, uint16ToFloat16 } from "@/utils/utils"
+
+import { ExportImageFormats } from "@/types.ts"
+
+import { Input } from "@/components/ui/input"
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+
+function flipVertically(imageData: ImageData) {
+  const width = imageData.width
+  const height = imageData.height
+  const data = imageData.data
+  const bytesPerPixel = 4 // RGBA
+  const rowSize = width * bytesPerPixel
+  const tempRow = new Uint8ClampedArray(rowSize)
+
+  for (let y = 0; y < height / 2; y++) {
+    const topRowStart = y * rowSize
+    const bottomRowStart = (height - 1 - y) * rowSize
+
+    // Save the top row to the temp row
+    tempRow.set(data.subarray(topRowStart, topRowStart + rowSize))
+
+    // Copy the bottom row to the top row
+    data.copyWithin(topRowStart, bottomRowStart, bottomRowStart + rowSize)
+
+    // Copy the saved top row to the bottom row
+    data.set(tempRow, bottomRowStart)
+  }
+
+  return imageData
+}
+
+const saveImage = async (filename: string, exportFormat: ExportImageFormats, exportQuality: number) => {
+  const prefs = usePreferenceStore.getState().prefs
+  const gl = Application.gl
+
+  const displayLayer = ResourceManager.get("DisplayLayer")
+
+  DrawingManager.pauseDrawNextFrame()
+  DrawingManager.clearSpecific(displayLayer)
+  DrawingManager.recomposite()
+  DrawingManager.render()
+  gl.bindFramebuffer(gl.FRAMEBUFFER, displayLayer.bufferInfo.framebuffer)
+
+  const glReadbackFormat = gl.getParameter(gl.IMPLEMENTATION_COLOR_READ_FORMAT) as number
+  const glReadbackType = gl.getParameter(gl.IMPLEMENTATION_COLOR_READ_TYPE) as number
+
+  gl.readBuffer(gl.COLOR_ATTACHMENT0)
+
+  const data = new Uint16Array(prefs.canvasWidth * prefs.canvasHeight * 4)
+  await readPixelsAsync(gl, 0, 0, prefs.canvasWidth, prefs.canvasHeight, glReadbackFormat, glReadbackType, data)
+
+  // Data is 16 bit float values stored in a uint16 array
+  const data8bit = Uint8ClampedArray.from(data, (num) => {
+    return uint16ToFloat16(num) * 255
+  })
+
+  const imageData = new ImageData(data8bit, prefs.canvasWidth, prefs.canvasHeight)
+
+  flipVertically(imageData)
+
+  const imageBitmap = await createImageBitmap(imageData)
+  Application.exportCanvasContext.transferFromImageBitmap(imageBitmap)
+
+  const exportFormatMIME = getMIMEFromImageExtension(exportFormat)
+
+  const blob = await Application.exportCanvas.convertToBlob({ type: exportFormatMIME, quality: exportQuality })
+
+  if (!blob) {
+    console.error("Unable to create blob and save image")
+    return
+  }
+
+  const fullFilename = `${filename}.${exportFormat}`
+
+  const fileData = new File([blob], fullFilename, { type: exportFormatMIME })
+  const url = URL.createObjectURL(fileData)
+
+  Application.exportDownloadLink.setAttribute("href", url)
+  Application.exportDownloadLink.download = fullFilename
+  Application.exportDownloadLink.click()
+
+  URL.revokeObjectURL(url)
+}
+
+export function ExportDialog() {
+  const [quality, setQuality] = useState(1)
+  const [filename, setFilename] = useState("export")
+  const [format, setFormat] = useState(Application.supportedExportImageFormats[0]) // PNG is always supported
+
+  return (
+    <DialogHeader>
+      <DialogTitle>Save Image</DialogTitle>
+      <div className="flex flex-row items-center justify-between">
+        {SettingSlider("Quality", quality, (value) => setQuality(value), 1, { min: 0, max: 1, step: 0.1 })}
+
+        <Input
+          type="string"
+          className="w-[15ch]"
+          placeholder="Your Filename"
+          value={filename}
+          onChange={(event) => setFilename(event.target.value)}
+        />
+
+        <Select defaultValue={format} onValueChange={(value) => setFormat(value as ExportImageFormats)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Format" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {Application.supportedExportImageFormats.map((imageFormat, index) => (
+                <SelectItem key={`exportFormat${index}`} value={imageFormat}>
+                  {imageFormat.toUpperCase()}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <Button variant="outline" size="sm" className="" onClick={() => void saveImage(filename, format, quality)}>
+        Save Image
+      </Button>
+    </DialogHeader>
+  )
+}
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import {
   Menubar,
   MenubarContent,
@@ -9,108 +11,76 @@ import {
 
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 
-import { ResourceManager } from "@/managers/ResourceManager"
-import { Slider } from "@/components/ui/slider"
+import { SettingSlider } from "@/components/SettingSlider"
 import { usePreferenceStore } from "@/stores/PreferenceStore"
 import { DrawingManager } from "@/managers/DrawingManager"
-import { Application } from "@/managers/ApplicationManager"
+import { ExportDialog } from "@/components/ExportDialog"
 
-import { readPixelsAsync } from "@/utils/asyncReadback"
-
-import { uint16ToFloat16 } from "@/utils/utils"
-
-function flipVertically(imageData: ImageData) {
-  const width = imageData.width
-  const height = imageData.height
-  const data = imageData.data
-  const bytesPerPixel = 4 // RGBA
-  const rowSize = width * bytesPerPixel
-  const tempRow = new Uint8ClampedArray(rowSize)
-
-  for (let y = 0; y < height / 2; y++) {
-    const topRowStart = y * rowSize
-    const bottomRowStart = (height - 1 - y) * rowSize
-
-    // Save the top row to the temp row
-    tempRow.set(data.subarray(topRowStart, topRowStart + rowSize))
-
-    // Copy the bottom row to the top row
-    data.copyWithin(topRowStart, bottomRowStart, bottomRowStart + rowSize)
-
-    // Copy the saved top row to the bottom row
-    data.set(tempRow, bottomRowStart)
-  }
-
-  return imageData
-}
-
-const saveImage = async () => {
-  const prefs = usePreferenceStore.getState().prefs
-  const gl = Application.gl
-
-  const displayLayer = ResourceManager.get("DisplayLayer")
-
-  DrawingManager.pauseDrawNextFrame()
-  DrawingManager.clearSpecific(displayLayer)
-  DrawingManager.recomposite()
-  DrawingManager.render()
-  gl.bindFramebuffer(gl.FRAMEBUFFER, displayLayer.bufferInfo.framebuffer)
-
-  const format = gl.getParameter(gl.IMPLEMENTATION_COLOR_READ_FORMAT) as number
-  const type = gl.getParameter(gl.IMPLEMENTATION_COLOR_READ_TYPE) as number
-
-  gl.readBuffer(gl.COLOR_ATTACHMENT0)
-
-  const data = new Uint16Array(prefs.canvasWidth * prefs.canvasHeight * 4)
-  await readPixelsAsync(gl, 0, 0, prefs.canvasWidth, prefs.canvasHeight, format, type, data)
-
-  // Data is 16 bit float values stored in a uint16 array
-  const data8bit = Uint8ClampedArray.from(data, (num) => {
-    return uint16ToFloat16(num) * 255
-  })
-
-  const imageData = new ImageData(data8bit, prefs.canvasWidth, prefs.canvasHeight)
-
-  flipVertically(imageData)
-
-  const imageBitmap = await createImageBitmap(imageData)
-  Application.exportCanvasContext.transferFromImageBitmap(imageBitmap)
-
-  const blob = await Application.exportCanvas.convertToBlob({ type: "image/png", quality: 1.0 })
-
-  if (!blob) {
-    console.error("Unable to create blob and save image")
-    return
-  }
-
-  const fileData = new File([blob], "image.png", { type: "png" })
-  const url = URL.createObjectURL(fileData)
-
-  Application.exportDownloadLink.setAttribute("href", url)
-  Application.exportDownloadLink.download = "image.png"
-  Application.exportDownloadLink.click()
-
-  URL.revokeObjectURL(url)
-}
-
-const SliderSetting = (name: string, value: number, _onValueChange: (value: number) => void, props: any) => {
-  const onValueChange = (value: number[]) => _onValueChange(value[0]) // Radix UI uses values in arrays to support multiple thumbs
-  // const handler = useCallback(throttle(onValueChange, 16), [dependency])
+const PreferenesDialog = () => {
+  const setPrefs = usePreferenceStore.use.setPrefs()
+  const prefs = usePreferenceStore.use.prefs()
 
   return (
-    <div key={`${name}_setting`} className="flex h-full flex-row items-center justify-center pt-2">
-      <div className="flex h-full flex-row items-center justify-center">
-        <p className="pr-2 text-sm text-muted-foreground">{name}</p>
-        <Slider className="mr-4 w-28" {...props} value={[value]} onValueChange={onValueChange} />
-        <p className="mr-2 w-[3ch] text-sm text-muted-foreground">{value.toFixed(2)}</p>
-      </div>
-    </div>
+    <DialogHeader>
+      <DialogTitle>Preferences</DialogTitle>
+      {SettingSlider(
+        "Pressure Sensitivity",
+        prefs.pressureSensitivity,
+        (pressureSensitivity) => setPrefs({ pressureSensitivity }),
+        2,
+        {
+          min: 0,
+          max: 1,
+          step: 0.01,
+        },
+      )}
+
+      {SettingSlider(
+        "Pressure Filtering",
+        ((1 - prefs.pressureFiltering) * 10) / 10,
+        (pressureFiltering) => setPrefs({ pressureFiltering: 1 - pressureFiltering }),
+        2,
+        {
+          min: 0,
+          max: 0.99,
+          step: 0.01,
+        },
+      )}
+
+      {SettingSlider(
+        "Mouse Filtering",
+        ((1 - prefs.mouseFiltering) * 10) / 10,
+        (mouseFiltering) => setPrefs({ mouseFiltering: 1 - mouseFiltering }),
+        2,
+        {
+          min: 0,
+          max: 0.99,
+          step: 0.01,
+        },
+      )}
+
+      {SettingSlider(
+        "Mouse Smoothing",
+        ((1 - prefs.mouseSmoothing) * 10) / 10,
+        (mouseSmoothing) => setPrefs({ mouseSmoothing: 1 - mouseSmoothing }),
+        2,
+        {
+          min: 0,
+          max: 0.99,
+          step: 0.01,
+        },
+      )}
+    </DialogHeader>
   )
 }
 
+enum Dialogs {
+  export = 0,
+  preferences,
+}
+
 function _TopMenu() {
-  const setPrefs = usePreferenceStore.use.setPrefs()
-  const prefs = usePreferenceStore.use.prefs()
+  const [currentDialog, setCurrentDialog] = useState(Dialogs.export)
 
   return (
     <>
@@ -120,7 +90,10 @@ function _TopMenu() {
             <MenubarTrigger>File</MenubarTrigger>
             <MenubarContent>
               <MenubarItem disabled>New</MenubarItem>
-              <MenubarItem onClick={() => void saveImage()}>Save Image</MenubarItem>
+              <DialogTrigger asChild onClick={() => setCurrentDialog(Dialogs.export)}>
+                <MenubarItem>Save Image</MenubarItem>
+              </DialogTrigger>
+
               <MenubarItem disabled>Exit</MenubarItem>
             </MenubarContent>
           </MenubarMenu>
@@ -133,7 +106,7 @@ function _TopMenu() {
               <MenubarItem onSelect={() => DrawingManager.clearAll()}>Clear All</MenubarItem>
               <MenubarSeparator />
               <MenubarItem disabled>Project Name</MenubarItem>
-              <DialogTrigger asChild>
+              <DialogTrigger asChild onClick={() => setCurrentDialog(Dialogs.preferences)}>
                 <MenubarItem>Preferences</MenubarItem>
               </DialogTrigger>
             </MenubarContent>
@@ -153,52 +126,8 @@ function _TopMenu() {
           </MenubarMenu>
         </Menubar>
         <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Preferences</DialogTitle>
-            {SliderSetting(
-              "Pressure Sensitivity",
-              prefs.pressureSensitivity,
-              (pressureSensitivity) => setPrefs({ pressureSensitivity }),
-              {
-                min: 0,
-                max: 1,
-                step: 0.01,
-              },
-            )}
-
-            {SliderSetting(
-              "Pressure Filtering",
-              ((1 - prefs.pressureFiltering) * 10) / 10,
-              (pressureFiltering) => setPrefs({ pressureFiltering: 1 - pressureFiltering }),
-              {
-                min: 0,
-                max: 0.99,
-                step: 0.01,
-              },
-            )}
-
-            {SliderSetting(
-              "Mouse Filtering",
-              ((1 - prefs.mouseFiltering) * 10) / 10,
-              (mouseFiltering) => setPrefs({ mouseFiltering: 1 - mouseFiltering }),
-              {
-                min: 0,
-                max: 0.99,
-                step: 0.01,
-              },
-            )}
-
-            {SliderSetting(
-              "Mouse Smoothing",
-              ((1 - prefs.mouseSmoothing) * 10) / 10,
-              (mouseSmoothing) => setPrefs({ mouseSmoothing: 1 - mouseSmoothing }),
-              {
-                min: 0,
-                max: 0.99,
-                step: 0.01,
-              },
-            )}
-          </DialogHeader>
+          {currentDialog === Dialogs.preferences ? <PreferenesDialog /> : null}
+          {currentDialog === Dialogs.export ? <ExportDialog /> : null}
         </DialogContent>
       </Dialog>
     </>
