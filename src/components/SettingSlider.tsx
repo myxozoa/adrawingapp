57f1dabcc1015a179f66ff002bf122baf9c35983
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-05-29 05:25:37 -0700

    Use label component in slider

--- src/components/SettingSlider.tsx
+++ src/components/SettingSlider.tsx
@@ -2,6 +2,8 @@ import { Slider } from "@/components/ui/slider"
 import { compareProps } from "@/utils/utils"
 import { SliderProps } from "@radix-ui/react-slider"
 
+import { Label } from "@/components/ui/label"
+
 import { useCallback } from "react"
 
 import { memo } from "react"
@@ -18,9 +20,19 @@ function _SettingSlider({ name, value, onValueChange, fractionDigits, ...props }
 
   return (
     <div key={`${name}_setting`} className="flex w-fit flex-row items-center justify-center">
-      <p className="pr-2 text-sm text-muted-foreground">{name}</p>
-      <Slider className="mr-2 w-28" {...props} value={[value]} onValueChange={valueChange} />
-      <p className="w-[3ch] text-sm text-muted-foreground">{value.toFixed(fractionDigits)}</p>
+      <Label className="pr-2" htmlFor={`setting_slider_${name}`}>
+        {name}
+      </Label>
+      <Slider
+        id={`setting_slider_${name}`}
+        className="mr-2 w-28"
+        {...props}
+        value={[value]}
+        onValueChange={valueChange}
+      />
+      <Label className="w-[3ch]" htmlFor={`setting_slider_${name}`}>
+        {value.toFixed(fractionDigits)}
+      </Label>
     </div>
   )
 }
