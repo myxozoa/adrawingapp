b4a31c331ed0c35d1cf708557f79b98add256305
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-05-29 21:55:33 -0700

    option to hide text in settings slider

--- src/components/SettingSlider.tsx
+++ src/components/SettingSlider.tsx
@@ -11,18 +11,21 @@ import { memo } from "react"
 interface SettingSliderProps extends Omit<SliderProps, "value" | "onValueChange"> {
   name: string
   value: number
+  hideText?: boolean
   onValueChange: (value: number) => void
   fractionDigits: number
 }
 
-function _SettingSlider({ name, value, onValueChange, fractionDigits, ...props }: SettingSliderProps) {
+function _SettingSlider({ name, value, hideText, onValueChange, fractionDigits, ...props }: SettingSliderProps) {
   const valueChange = useCallback((value: number[]) => onValueChange(value[0]), []) // Radix UI uses values in arrays to support multiple thumbs
 
   return (
     <div key={`${name}_setting`} className="flex w-fit flex-row items-center justify-center">
-      <Label className="pr-2" htmlFor={`setting_slider_${name}`}>
-        {name}
-      </Label>
+      {!hideText ? (
+        <Label className="pr-2" htmlFor={`setting_slider_${name}`}>
+          {name}
+        </Label>
+      ) : null}
       <Slider
         id={`setting_slider_${name}`}
         className="mr-2 w-28"
@@ -30,9 +33,11 @@ function _SettingSlider({ name, value, onValueChange, fractionDigits, ...props }
         value={[value]}
         onValueChange={valueChange}
       />
-      <Label className="w-[3ch]" htmlFor={`setting_slider_${name}`}>
-        {value.toFixed(fractionDigits)}
-      </Label>
+      {!hideText ? (
+        <Label className="w-[3ch]" htmlFor={`setting_slider_${name}`}>
+          {value.toFixed(fractionDigits)}
+        </Label>
+      ) : null}
     </div>
   )
 }
