6b1dd148dabfd452cf7555fe573711e3464dc726
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-05-29 21:56:02 -0700

    normal font for labels

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const labelVariants = cva(
-  "text-sm leading-none text-muted-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+  "text-sm font-normal leading-none text-muted-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
 )
 
 const Label = React.forwardRef<
