815f7009e6456d5d8f3e9126c5a595f72633a188
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-06-06 23:16:04 -0700

    cant reset ids because the layers are being reused somehow

--- src/objects/Layer.ts
+++ src/objects/Layer.ts
@@ -63,7 +63,6 @@ export class Layer implements ILayer {
     this.boundingBox = { x: 0, y: 0, width: 1, height: 1 } //  TODO: Calculate every time drawn to
     this.opacity = 100
     this.drawnTo = false
-    this.id = crypto.randomUUID ? crypto.randomUUID() : (Math.random() * Math.random()).toString()
   }
 
   // addCurrentToUndoSnapshotQueue = (gl: WebGL2RenderingContext) => {
