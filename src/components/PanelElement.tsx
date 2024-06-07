75d765cc407eb4bc2c22fe8afbae0e4496f0158e
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-06-06 23:14:20 -0700

    removing panel element outlines

--- src/components/PanelElement.tsx
+++ src/components/PanelElement.tsx
@@ -20,9 +20,7 @@ function _PanelElement({
     <div
       onDoubleClick={onDoubleClick}
       onClick={() => select(id)}
-      className={`w-full rounded-sm p-0.5 hover:cursor-pointer ${className} ${
-        selected ? "bg-secondary outline outline-1 outline-secondary-foreground" : ""
-      }`}
+      className={`w-full rounded-sm p-0.5 hover:cursor-pointer ${className} ${selected ? "bg-secondary" : ""}`}
     >
       <div className="flex h-full items-center justify-center">{children}</div>
     </div>
