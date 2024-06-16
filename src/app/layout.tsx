58174d2009071e934b6ec7e4977185639549b6f9
Ronnie Miksch <32121076+myxozoa@users.noreply.github.com>
2024-06-16 05:38:32 -0700

    swapping to inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next"
 
+import { Inter } from "next/font/google"
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter", display: "swap" })
+
 export const metadata: Metadata = {
   title: "A Drawing App",
   description: "Digital Painting Application",
@@ -7,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <div id="root">{children}</div>
         <a id="local_filesaver" target="_blank" rel="noopener" className="hidden"></a>
